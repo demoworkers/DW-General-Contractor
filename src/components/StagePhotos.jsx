16a843ca995e5d557e90e5d838e137aefb84cf00
Osama A. Shahi <osamashahi.os@gmail.com>
2022-12-11 02:32:39 +0500

    photos updates

--- src/components/StagePhotos.jsx
+++ src/components/StagePhotos.jsx
@@ -5,17 +5,66 @@ import { ProgressBar } from 'primereact/progressbar'
 import { Button } from 'primereact/button'
 import { Tooltip } from 'primereact/tooltip'
 import { Tag } from 'primereact/tag'
+import { XCircleIcon } from '@heroicons/react/20/solid'
 
 export const StagePhotos = () => {
+  const [totalSize, setTotalSize] = useState(0)
   const toast = useRef(null)
   const fileUploadRef = useRef(null)
 
-  const onUpload = () => {
-    toast.current.show({
-      severity: 'info',
-      summary: 'Success',
-      detail: 'File Uploaded',
-    })
+  const headerTemplate = (options) => {
+    const { className, chooseButton, uploadButton, cancelButton } = options
+    const value = totalSize / 10000
+    const formatedValue =
+      fileUploadRef && fileUploadRef.current
+        ? fileUploadRef.current.formatSize(totalSize)
+        : '0 B'
+
+    return (
+      <div
+        className={className}
+        style={{
+          backgroundColor: 'transparent',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        {chooseButton}
+        {/* {uploadButton}
+        {cancelButton} */}
+        {/* <ProgressBar
+          value={value}
+          displayValueTemplate={() => `${formatedValue} / 1 MB`}
+          style={{ width: '300px', height: '20px', marginLeft: 'auto' }}
+        ></ProgressBar> */}
+      </div>
+    )
+  }
+
+  const onTemplateRemove = (file, callback) => {
+    setTotalSize(totalSize - file.size)
+    callback()
+  }
+
+  const itemTemplate = (file, props) => {
+    return (
+      <div className="flex flex-wrap items-center">
+        <div className="relative flex items-center">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt={file.name}
+            role="presentation"
+            src={file.objectURL}
+            width={100}
+            className="border rounded-md"
+          />
+          <XCircleIcon
+            className="absolute top-0 right-0 -top-1.5 -right-1.5 w-5 cursor-pointer"
+            onClick={() => onTemplateRemove(file, props.onRemove)}
+          />
+        </div>
+      </div>
+    )
   }
 
   return (
@@ -25,15 +74,22 @@ export const StagePhotos = () => {
       </div>
       <Toast ref={toast} />
       <FileUpload
+        ref={fileUploadRef}
         name="files[]"
         url="http://localhost:3000/api/uploadFile"
-        onUpload={onUpload}
         multiple
         accept="image/*"
         maxFileSize={1000000}
-        emptyTemplate={
-          <p className="m-0">Drag and drop files to here to upload.</p>
-        }
+        // onUpload={onTemplateUpload}
+        // onSelect={onTemplateSelect}
+        // onError={onTemplateClear}
+        // onClear={onTemplateClear}
+        headerTemplate={headerTemplate}
+        itemTemplate={itemTemplate}
+        // emptyTemplate={emptyTemplate}
+        // chooseOptions={chooseOptions}
+        // uploadOptions={uploadOptions}
+        // cancelOptions={cancelOptions}
       />
     </>
   )
